3433a56026b479cf2d1e7fbe313a6a913eb32465
Kayode Sonaike <Okayode97@gmail.com>
2025-02-23 09:47:36 +0000

    - updated draw_bbox_onto_frame_function to include colour arg - added functions written from experiments with SIFT feature extraction   and matching

--- cv_detectors.py
+++ cv_detectors.py
@@ -1,14 +1,16 @@
 import cv2
 import numpy as np
 from pathlib import Path
-from typing import Optional, Tuple, List
+from typing import Optional
 from dataclasses import dataclass
+import time
 
 
 class video_reader:
     """
     class written to get frame from video source.
-    wrote this mainly to be able to get specific frames from camera and to have a nice wrapper around cv2.VideoCapture.
+    wrote this mainly to be able to get specific frames from camera and to have a nice wrapper around
+    cv2.VideoCapture.
 
     Args:
         - video_source (Path | str | int): Source for video
@@ -47,7 +49,7 @@ class video_reader:
         if self.is_live:
             raise RuntimeError("Running from live feed..., unable to retrieve specific frames")
         
-        # raise error if provided error is out of range
+        # raise error if provided index is out of range
         if (index < 0) or (index >= self.total_frames):
             raise IndexError(f"Frame index {index} out of range (0 to {self.total_frames-1}).")
 
@@ -70,10 +72,11 @@ class bbox:
     height: int
     width: int
 
-    def draw_bbox_onto_frame(self, frame: np.ndarray) -> np.ndarray:
+    def draw_bbox_onto_frame(self, frame: np.ndarray, colour: tuple[int]=(0, 255, 0)) -> np.ndarray:
         # rectangle drawn using top left cornor to bottom right cornor
         # color in BGR
-        frame = cv2.rectangle(frame, (self.x, self.y), (self.x + self.width, self.y + self.height), (0, 255, 0), 2)
+        frame = cv2.rectangle(frame, (self.x, self.y), (self.x + self.width, self.y + self.height),
+                colour, 2)
         return frame
 
 def image_preprocessing(frame: np.ndarray) -> np.ndarray:
@@ -85,7 +88,7 @@ def image_preprocessing(frame: np.ndarray) -> np.ndarray:
     return resized_frame
 
 
-def get_minimum_bbox_that_fits_each_circle(hough_circles: np.ndarray) -> List[bbox]:
+def get_minimum_bbox_that_fits_each_circle(hough_circles: np.ndarray) -> list[bbox]:
     list_of_bbox = []
     for circles in hough_circles:
         x = circles[0] - circles[2]
@@ -96,7 +99,7 @@ def get_minimum_bbox_that_fits_each_circle(hough_circles: np.ndarray) -> List[bb
     return list_of_bbox
     
 
-def houghcircle_detections(frame: np.ndarray) -> List[bbox]:
+def houghcircle_detections(frame: np.ndarray) -> list[bbox]:
     greyscale_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # circles is a list of (x, y, r) extra dims added
@@ -110,14 +113,14 @@ def houghcircle_detections(frame: np.ndarray) -> List[bbox]:
     return []
 
 
-def draw_bounding_box(frame: np.ndarray, list_of_bbox: List[bbox]) -> np.ndarray:
+def draw_bounding_box(frame: np.ndarray, list_of_bbox: list[bbox], colour: tuple[int]) -> np.ndarray:
     for box in list_of_bbox:
         # draw bounding box onto frame
-        frame = box.draw_bbox_onto_frame(frame)
+        frame = box.draw_bbox_onto_frame(frame, colour)
     return frame
 
 
-def colour_based_segmentation(frame: np.ndarray, min_contour_area: int = 500) -> List[bbox]:
+def colour_based_segmentation(frame: np.ndarray, min_contour_area: int = 500) -> list[bbox]:
     # convert to hsv colour space
     hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -144,6 +147,52 @@ def colour_based_segmentation(frame: np.ndarray, min_contour_area: int = 500) ->
     return list_of_bbox
 
 
+def roi_selector(frame: np.ndarray):
+    # contains y, x, w, h
+    roi = cv2.selectROI("select ROI", frame, False)
+
+    # if valid roi is returned write the cropped image to file
+    if roi != (0, 0, 0, 0):
+        cropped_image = frame[int(roi[1]):int(roi[1]+roi[3]),  
+                            int(roi[0]):int(roi[0]+roi[2])] 
+        
+        cv2.imwrite("target_img.png", cropped_image)
+
+
+def instantiate_sift_and_feature_matcher():
+    # sift feature extractor
+    sift = cv2.SIFT_create()
+
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks = 50)
+
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    return sift, flann
+
+
+def experimenting_with_feature_matching(src_img: np.ndarray, tgt_img: np.ndarray,
+                                        sift, flann) -> Optional[np.ndarray]:
+
+    src_img = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
+
+    min_match_count = 10
+    kp1, des1 = sift.detectAndCompute(src_img, None)
+    kp2, des2 = sift.detectAndCompute(tgt_img, None)
+
+    matches = flann.knnMatch(des1, des2, k=2)
+    
+    # store all the good matches as per Lowe's ratio test.
+    good = []
+    for m,n in matches:
+        if m.distance < 0.7*n.distance:
+            good.append(m)
+    
+    if len(good)>min_match_count:
+        src_pts = np.rint([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,2).astype(int)
+        return src_pts
+
+
 """
 Different opencv detectors to consider
 - cv2.HoughCircles (with min/max radius), with edge detection. key parameters
@@ -161,24 +210,43 @@ Different opencv detectors to consider
     (high value -> fewer but more accurate circle,
     low value -> more circle detected but possibly more false positives)
 
-- cv2 colour based segmentation (colour thresholding to detect the gloves in the image.
+- cv2 colour based segmentation (colour thresholding to detect the gloves in the image).
     -
 - Possibly other crafted features to detect the bottle of chemical.
+    - considered using template matcher, which tries to find a template image from the live image,
+      this approach works well for static image but is limited once the object starts to move.
+    - experimenting with using crafted features and feature matching. Similar to the considered 
+      approach with template matcher, it requires providing the target image with features to extract
+      and then find and extract it from the live image.
 
 """
 
 video = video_reader(r"AICandidateTest-FINAL.mp4")
-frame = video.get_frame(2678) # also sets the starting frame
+frame = video.get_frame(0) # also sets the starting frame
+
+sift, flann_matcher = instantiate_sift_and_feature_matcher()
+
+# target image
+target_img = cv2.imread("target_img.png", cv2.IMREAD_GRAYSCALE)
 
 for frame in video:
     frame = image_preprocessing(frame)
-
+ 
+    start_time = time.time()
     list_of_houghcircle_bbox = houghcircle_detections(frame)
     list_of_color_based_bbox = colour_based_segmentation(frame, 500)
 
-    frame = draw_bounding_box(frame, list_of_color_based_bbox+list_of_houghcircle_bbox)
+    duration = time.time() - start_time
+    if duration > 0:
+        fps = 1/duration
+        print(f"Detectors fps: {int(fps)} | frame processed: {video.frame_number}/{len(video)}",
+        f"| number of detections: {len(list_of_houghcircle_bbox)+len(list_of_color_based_bbox)}")
+
+    frame = draw_bounding_box(frame, list_of_color_based_bbox, (255, 0, 0))
+    frame = draw_bounding_box(frame, list_of_houghcircle_bbox, (0, 255, 0))
 
     # Display result
     cv2.imshow("Lab technician object detection and tracking", frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
