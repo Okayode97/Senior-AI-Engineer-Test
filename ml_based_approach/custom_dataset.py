- updated gitignore to ignore hidden folder - corrected errors with box annotations extracted from annotations json   file - updated image preprocessing (normalisation (0-1) and type casting as   float) - removed image preprocessing func and used train_transform func - added function to test data loader to confirm correct box annotations - tried reducing learning rate - defined model weights as constant - made adjustment to image preprocessing function and get model function - got model training working properly

ml_based_approach/custom_dataset.py
@@ -6,10 +6,12 @@ import cv2
 import os
 from collections import defaultdict
 import torchvision.transforms as transforms
+from torchvision.io import read_image
+import torchvision.transforms as T
 import numpy as np
 
 class LabDataset(Dataset):
-    def __init__(self, dataset_folder: str | Path, annotation: str | Path, preprocessing_func=None):        
+    def __init__(self, dataset_folder: str | Path, annotation: str | Path, preprocessing_func=None):
         self.dataset_folder = dataset_folder
         self.annotations = self.load_json_data(annotation)
         self.preprocess_func = preprocessing_func
@@ -33,7 +35,12 @@ class LabDataset(Dataset):
         # get the data associated with the idx
         image_dict = self.annotations["images"][idx]
         img_path = os.path.join(self.dataset_folder, image_dict["file_name"])
+
+        # image h, w, c
+        # image = read_image(img_path)
         image = cv2.imread(img_path)
+        image = image.astype(np.float32) # specify dtype
+        image /= 255 # normalise to range from 0 - 1
 
         if image is None:
             raise ValueError("Image read as none")
@@ -46,9 +53,9 @@ class LabDataset(Dataset):
         labels = []
         for annotation in annotation_list:
             # convert bounding box format x,y,w,h to x,y,xy
-            x_min, y_min, height, width = annotation['bbox']
-            x_max = x_min + height
-            y_max = y_min + width
+            y_min, x_min, height, width = annotation['bbox']
+            x_max = x_min + width + 10 # add 10px to height and width to better cover object
+            y_max = y_min + height + 10
             boxes.append([x_min, y_min, x_max, y_max])
             labels.append(annotation['category_id'])
 
@@ -70,8 +77,8 @@ class LabDataset(Dataset):
             image = self.preprocess_func(image)
         return image, target
 
-def image_preprocessing(frame: np.ndarray) -> torch.tensor:    
-    # add color dimension first, normalise range and expand dims to include batch dimension
-    frame = frame.transpose((2, 0, 1))
-    frame = frame / 255
-    return torch.from_numpy(frame).float() # set type to float
+
+train_transforms = T.Compose([
+    T.ToTensor()
+    # T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # normalisation result in unsuable images
+])

ml_based_approach/model_finetuning.py
@@ -4,8 +4,9 @@ import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
-from custom_dataset import LabDataset, image_preprocessing
-
+from custom_dataset import LabDataset, train_transforms
+from torchvision.utils import draw_bounding_boxes
+import cv2
 
 # training config
 # possible TODO, turn this into a dataclass and experiment with changing different parameters
@@ -13,10 +14,11 @@ from custom_dataset import LabDataset, image_preprocessing
 # moved training to google colab
 NUM_CLASSES = 3  # 0 - background, 1 - petri_dish, 2 gloves
 BATCH_SIZE = 4
-LEARNING_RATE = 0.009
+LEARNING_RATE = 0.005
 EPOCHS = 40
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 ANNOTATION_JSON_FILE = r"/content/drive/MyDrive/test/dataset_v2/annotations.json"
+MODEL_WEIGHT_NAME = "faster_rcnn_custom.pth"
 
 
 # load pre-trained model and set num of clas
@@ -24,7 +26,6 @@ def get_model():
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights="COCO_V1")
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, NUM_CLASSES)
-    model.to(DEVICE)
     return model
 
 
@@ -32,6 +33,17 @@ def get_model():
 def collate_fn(batch):
     return tuple(zip(*batch))
 
+def test_dataloader(data_loader):
+    for images, targets in data_loader:
+        for image, target in zip(images, targets):
+            # set image to H, W, C
+            boxes = target["boxes"]
+            image = draw_bounding_boxes(image, boxes, width=5)
+
+            # convert to numpy array and transpose back to color last
+            image = image.detach().numpy().transpose((1, 2, 0))
+            cv2.imshow("annotations", image)
+            cv2.waitKey(0)
 
 # training function
 def train_one_epoch(model, optimizer, data_loader, device, epoch):
@@ -92,26 +104,31 @@ class EarlyStopper:
         return False
 
 # load the dataset
-dataset = LabDataset(r"/content/drive/MyDrive/test/dataset_v2", r"/content/drive/MyDrive/test/dataset_v2/annotations.json", image_preprocessing)
+full_dataset = LabDataset(r"C:\Users\oluka\Desktop\Job Application 2025\Reach industries\Senior-AI-Engineer-Test\ml_based_approach\.dataset_v2",
+                          r"C:\Users\oluka\Desktop\Job Application 2025\Reach industries\Senior-AI-Engineer-Test\ml_based_approach\.dataset_v2\annotations.json",
+                          train_transforms)
 
 # divide the dataset into training, validation & test set
-train_size = int(0.8 * len(dataset))
-validation_size = len(dataset) - train_size
-dataset, validation_test = torch.utils.data.random_split(dataset, [train_size, validation_size])
+train_size = int(0.8 * len(full_dataset))
+validation_size = len(full_dataset) - train_size
+dataset, validation_set = torch.utils.data.random_split(full_dataset, [train_size, validation_size])
 
 # define training and test data loaders
 train_loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_fn)
-val_loader = DataLoader(validation_test, batch_size=BATCH_SIZE, shuffle=False, collate_fn=collate_fn)
+val_loader = DataLoader(validation_set, batch_size=BATCH_SIZE, shuffle=False, collate_fn=collate_fn)
+
+# test_dataloader(train_loader)
 
 # get custom model
-model = get_model()
+model = get_model().to(DEVICE)
+model.train()
 
 # construct optimizers, learning rate scheduler
 params = [p for p in model.parameters() if p.requires_grad]
 optimizer = torch.optim.SGD(params, lr=LEARNING_RATE, momentum=0.9, weight_decay=0.0005)
 lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1) 
 
-early_stopper = EarlyStopper(patience=4, min_delta=0.05)
+early_stopper = EarlyStopper(patience=4, min_delta=0.005)
 
 # train the model and evaluate it using the validation set
 for epoch in range(EPOCHS):
@@ -122,4 +139,4 @@ for epoch in range(EPOCHS):
     if early_stopper.early_stop(val_loss):
         break
 
-torch.save(model.state_dict(), "faster_rcnn_custom.pth")
+torch.save(model.state_dict(), MODEL_WEIGHT_NAME)

ml_based_approach/run_inference.py
@@ -11,7 +11,7 @@ from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 NUM_CLASSES = 3  # 0 - background, 1 - petri_dish, 2 gloves
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-MODEL_WEIGHT = r".\faster_rcnn_custom_2.pth"
+MODEL_WEIGHT = r"C:\Users\oluka\Desktop\Job Application 2025\Reach industries\Senior-AI-Engineer-Test\ml_based_approach\faster_rcnn_custom_4.pth"
 
 class video_reader:
     """
@@ -77,10 +77,13 @@ def image_preprocessing(frame: np.ndarray) -> torch.tensor:
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     
     # add color dimension first, normalise range and expand dims to include batch dimension
-    frame = frame.transpose((2, 0, 1))
+    frame = frame.astype(np.float32)
     frame = frame / 255
-    frame = np.expand_dims(frame, axis=0)
-    return torch.from_numpy(frame).float() # set type to float
+    frame = frame.transpose((2, 0, 1))
+
+    torch_tensor = torch.from_numpy(frame)
+    torch_tensor = torch.unsqueeze(torch_tensor, 0)
+    return torch_tensor # set type to float
 
 
 # load pre-trained model and set num of clas
@@ -88,7 +91,6 @@ def get_model() -> torchvision.models.detection.faster_rcnn.FasterRCNN:
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights="DEFAULT")
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, NUM_CLASSES)
-    model.to(DEVICE)
     return model
 
 
@@ -113,7 +115,7 @@ def top_n_detection(predictions: dict[torch.Tensor], confidence: float = 0.5) ->
 label_two_color = {1: (0, 255, 0), 2: (255, 0, 0)}
 
 # define the model
-model = get_model()
+model = get_model().to(DEVICE)
 
 # load the model weights and set it to evaluation mode 
 model.load_state_dict(torch.load(MODEL_WEIGHT, map_location=DEVICE, weights_only=True))
@@ -126,14 +128,12 @@ _ = video.get_frame(1000)
 for frame in video:
     if video.frame_number % 30 == 0:
         # preprocess each frame 
-        frame = image_preprocessing(frame)
+        torch_tensor = image_preprocessing(frame)
 
         start_time = time.time()
-        prediction = model(frame)[0] # account for batch dim
+        prediction = model(torch_tensor)[0] # account for batch dim
         duration = time.time() - start_time
 
-        # transpose frame (colour last) and convert back to a numpy array
-        frame = frame.detach().numpy()[0].transpose((1, 2, 0))
 
         if duration > 0:
             fps = 1/duration
@@ -141,17 +141,17 @@ for frame in video:
         
             frame = cv2.putText(frame, f'fps: {round(fps, 2)}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-
         # get predictions with score greater than given confidence
         selected_detections = top_n_detection(prediction, 0.7)
 
-
+        frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         for detection in selected_detections:
             box = detection["box"][0]
             label = detection["label"][0]
             score = detection["score"]
 
-            frame = cv2.rectangle(frame, (round(box[1]), round(box[0])), (round(box[3]), round(box[2])), label_two_color[label], 2)
+            frame = cv2.rectangle(frame, (round(box[0]), round(box[1])), (round(box[2]), round(box[3])), label_two_color[label], 2)
 
         cv2.imshow("Frame", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):